5190c04ddb81a7c1ed0b6602a5b6e8dbb9779bf5
jacob_NetEase <jkzhang818@163.com>
2018-02-05 20:07:07 +0800

    1

---
+++ src/go/jacob/day0204/Demo1.java
@@ -0,0 +1,7 @@
+package go.jacob.day0204;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		System.out.println("11");
+	}
+}
---
+++ src/go/jacob/day0204/Demo2.java
@@ -0,0 +1,5 @@
+package go.jacob.day0204;
+
+public class Demo2 {
+
+}
